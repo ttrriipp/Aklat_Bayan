45606c661dbb5c8c884102e427189b1da8f62045
freezy29 <markgarcia2904@gmail.com>
2024-12-01 14:38:16 +0800

    test

--- app/src/main/java/com/example/aklatbayan/UserFragment.java
+++ app/src/main/java/com/example/aklatbayan/UserFragment.java
@@ -1,5 +1,6 @@
 package com.example.aklatbayan;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +8,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class UserFragment extends Fragment {
+    Dialog logout;
+    private Button lgt, lgt2, cncl;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+        lgt = view.findViewById(R.id.btnLogout);
+        lgt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutpopup();
+            }
+        });
+        return view;
     }
+
+    private void logoutpopup() {
+        logout= new Dialog(requireContext(), R.style.Dialog_style);
+        logout.setContentView(R.layout.activity_downloads);
+        logout.getWindow().setBackgroundDrawableResource(R.drawable.viewtext);
+
+    }
+
+
 }
