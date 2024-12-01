679dafdbbcaa71e13a9faed0cf5040f2eda6f91f
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2024-12-01 17:42:16 +0800

    trip

--- app/src/main/java/com/example/aklatbayan/LoginPage.java
+++ app/src/main/java/com/example/aklatbayan/LoginPage.java
@@ -2,11 +2,9 @@ package com.example.aklatbayan;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,24 +12,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginPage extends AppCompatActivity {
     Button btnLogin;
-    public static final int styleAttributeXxx=0x7f04015a;
+    ImageButton btnBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottomBar), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         btnLogin = findViewById(R.id.btnLogin);
+        btnBack = findViewById(R.id.btnBack);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIt = new Intent(LoginPage.this, MainScreen.class);
                 startActivity(myIt);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
             }
         });
     }
--- app/src/main/java/com/example/aklatbayan/SignUpPage.java
+++ app/src/main/java/com/example/aklatbayan/SignUpPage.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,24 +14,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SignUpPage extends AppCompatActivity {
     Button btnSignup;
+    ImageButton btnBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottomBar), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         btnSignup = findViewById(R.id.btnSignUp);
+        btnBack = findViewById(R.id.btnBack);
 
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(SignUpPage.this, "You've successfully created an account", Toast.LENGTH_LONG).show();
-                Intent myIntent = new Intent(SignUpPage.this,MainActivity.class);
+                Intent myIntent = new Intent(SignUpPage.this, MainActivity.class);
                 startActivity(myIntent);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
             }
         });
     }
